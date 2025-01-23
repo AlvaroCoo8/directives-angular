e363b2c760cab899dc45c170686d327a04515065
AlvaroCoo8 <al.alvaro.coo.igeno@iesportada.org>
2025-01-23 10:34:12 +0100

    funcional counter

--- src/app/components/counter/counter.component.ts
+++ src/app/components/counter/counter.component.ts
@@ -3,10 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-components-counter',
   standalone: false,
-  
+
   templateUrl: './counter.component.html',
-  styleUrl: './counter.component.css'
+  styleUrl: './counter.component.css',
 })
 export class CounterComponent {
+  public counter: number = 0;
 
+  public increaseBy(value: number): void {
+    this.counter += value;
+  }
+
+  public reset(value: number): void {
+    this.counter = value;
+  }
 }
