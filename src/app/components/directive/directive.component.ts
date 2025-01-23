a72fdf5874018848b724545d32acdeba60f6f3c6
AlvaroCoo8 <al.alvaro.coo.igeno@iesportada.org>
2025-01-23 22:53:31 +0100

    completed funcionality directive

--- src/app/components/directive/directive.component.ts
+++ src/app/components/directive/directive.component.ts
@@ -3,10 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-components-directive',
   standalone: false,
-  
+
   templateUrl: './directive.component.html',
-  styleUrl: './directive.component.css'
+  styleUrl: './directive.component.css',
 })
 export class DirectiveComponent {
+  public title: string = 'Directive IF Component';
 
+  public pNgIf: string = 'Working with *ngIf';
+  public pIf: string = 'Working with @if';
+
+  public pVNgIF: string = 'Text displayed with ngIf directive';
+  public pVIF: string = 'Text displayed with If directive';
+
+  public displayTextNgIf: boolean = false;
+  public displayTextIf: boolean = false;
+
+  public toggleTextNgIf(text: string) {
+    this.displayTextNgIf = !this.displayTextNgIf;
+    this.displayTextIf = false;
+  }
+
+  public toggleTextIf(text: string) {
+    this.displayTextIf = !this.displayTextIf;
+    this.displayTextNgIf = false;
+  }
 }
