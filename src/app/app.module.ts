34c2d3b088036a360f2012eb098f4e477177ff2d
AlvaroCoo8 <al.alvaro.coo.igeno@iesportada.org>
2025-01-23 10:46:59 +0100

    add component directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { DirectiveComponent } from './components/directive/directive.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent
+    CounterComponent,
+    DirectiveComponent
   ],
   imports: [
     BrowserModule
