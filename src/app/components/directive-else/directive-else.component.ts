1dfa259176da73abe4dc54071c79fab26cdb83da
AlvaroCoo8 <al.alvaro.coo.igeno@iesportada.org>
2025-02-03 13:01:09 +0100

    completed funcionality directive-else

--- src/app/components/directive-else/directive-else.component.ts
+++ src/app/components/directive-else/directive-else.component.ts
@@ -7,6 +7,22 @@ import { Component } from '@angular/core';
   templateUrl: './directive-else.component.html',
   styleUrl: './directive-else.component.css'
 })
-export class DirectiveElseComponent {
 
+export class DirectiveElseComponent {
+  public title: string = 'Directive IF-ELSE Component';
+  public versionOld: string = 'by <17v';
+  public versionLatest: string = 'by >=17v';
+
+  public boolLoginOld: boolean = false;
+  public boolLoginLatest: boolean = false;
+
+  public loginOld(): void {
+    this.boolLoginOld = true;
+    this.boolLoginLatest = false;
+  }
+
+  public loginLatest(): void {
+    this.boolLoginLatest = true;
+    this.boolLoginOld = false;
+  }
 }
